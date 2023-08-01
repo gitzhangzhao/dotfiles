168add5d078f9a662b5f489ba3ad1d946fa64e57
gitzhangzhao <gitzhangzhao@gmail.com>
2023-08-01 23:41:56 +0800

    2023年 08月 01日 星期二 23:41:56 CST

--- polybar/.config/polybar/scripts/brightnessctl.sh
+++ polybar/.config/polybar/scripts/brightnessctl.sh
@@ -3,15 +3,15 @@ BRIGHTNESS_VALUE=`brightnessctl | grep -o "(.*" | tr -d "()"`
 BRIGHTNESS_NR=${BRIGHTNESS_VALUE//%}
 
 if [ $BRIGHTNESS_NR -lt 20 ]; then
-    BRIGHTNESS_ICON='%{T2} %{T-}'
+    BRIGHTNESS_ICON='%{F#ebcb8b}%{T2} %{F#eceff4}%{T-}'
 elif [ $BRIGHTNESS_NR -lt 40 ]; then
-    BRIGHTNESS_ICON='%{T2} %{T-}'
+    BRIGHTNESS_ICON='%{F#ebcb8b}%{T2} %{F#eceff4}%{T-}'
 elif [ $BRIGHTNESS_NR -lt 60 ]; then
-    BRIGHTNESS_ICON='%{T2} %{T-}'
+    BRIGHTNESS_ICON='%{F#ebcb8b}%{T2} %{F#eceff4}%{T-}'
 elif [ $BRIGHTNESS_NR -lt 80 ]; then
-    BRIGHTNESS_ICON='%{T2} %{T-}'
+    BRIGHTNESS_ICON='%{F#ebcb8b}%{T2} %{F#eceff4}%{T-}'
 else
-    BRIGHTNESS_ICON='%{T2} %{T-}'
+    BRIGHTNESS_ICON='%{F#ebcb8b}%{T2} %{F#eceff4}%{T-}'
 fi
 
 echo "$BRIGHTNESS_ICON $BRIGHTNESS_VALUE"
