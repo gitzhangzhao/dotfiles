b07e364dbf723fdbb647d31be3c9f3f9292d097d
gitzhangzhao <gitzhangzhao@gmail.com>
2025-01-08 10:47:53 +0800

    Wed Jan  8 10:47:53 CST 2025

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -278,16 +278,36 @@ return {
             config = true
         },
 
+        -- {
+        --     'danilamihailov/beacon.nvim',
+        --     event = 'BufReadPost',
+        --     opts = {
+        --         enable = true,
+        --         speed = 2,
+        --         width = 10,
+        --         highlight = { bg = 'pink' },
+        --         min_jump = 0
+        --     }
+        -- },
+
         {
-            'danilamihailov/beacon.nvim',
-            event = 'BufReadPost',
+            "sphamba/smear-cursor.nvim",
+            event = 'VeryLazy',
             opts = {
-                enable = true,
-                speed = 2,
-                width = 10,
-                highlight = { bg = 'pink' },
-                min_jump = 0
-            }
+                -- Smear cursor color. Defaults to Cursor GUI color if not set.
+                -- Set to "none" to match the text color at the target cursor position.
+                cursor_color = '#f5bde6',
+
+                -- Smear cursor when switching buffers or windows.
+                smear_between_buffers = true,
+
+                -- Smear cursor when moving within line or to neighbor lines.
+                smear_between_neighbor_lines = true,
+
+                -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+                -- Smears will blend better on all backgrounds.
+                legacy_computing_symbols_support = false,
+            },
         },
 
         {
